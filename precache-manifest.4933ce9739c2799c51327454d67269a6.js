4aafdba43950848cf955abb421d98725caded379
kkongnamul11 <kkongnamul11@naver.com>
2020-07-16 00:57:22 +0900

    Updates

---
+++ precache-manifest.4933ce9739c2799c51327454d67269a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18e8b34bc24735339d8bcd902f02d9b1",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "987f6c677f7eb10e016d",
+    "url": "/movie_app/static/css/main.7de99c6b.chunk.css"
+  },
+  {
+    "revision": "cdb9c670d58271bbd37d",
+    "url": "/movie_app/static/js/2.087c2ab0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.087c2ab0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "987f6c677f7eb10e016d",
+    "url": "/movie_app/static/js/main.b022b29a.chunk.js"
+  },
+  {
+    "revision": "aa3e532d568bd958a089",
+    "url": "/movie_app/static/js/runtime-main.979e280a.js"
+  }
+]);
